500b2a368deb1a46138ab0bd86094fd60e46827a
Roman Filippov <rf@romanfilippov.com>
2022-09-14 15:43:40 +0100

    fix: do not escape tokens

--- src/naming-strategy/naming-strategy.ts
+++ src/naming-strategy/naming-strategy.ts
@@ -1,3 +1,4 @@
+import { Token } from 'aws-cdk-lib';
 import { paramCase } from 'param-case';
 import { IStaticWebsitePreviewNamingToken } from './tokens';
 
@@ -13,14 +14,16 @@ export class StaticWebsitePreviewNamingStrategy implements IStaticWebsitePreview
    * Set a static name for your deployment.
    */
   static fromStaticName(name: string): IStaticWebsitePreviewNamingStrategy {
-    return new StaticWebsitePreviewNamingStrategy(name);
+    return new StaticWebsitePreviewNamingStrategy(paramCase(name));
   }
 
   /**
    * Crafts a name from an array of token values.
    */
   static fromTokens(...tokens: IStaticWebsitePreviewNamingToken[]): IStaticWebsitePreviewNamingStrategy {
-    return new StaticWebsitePreviewNamingStrategy(tokens.map(({ token }) => token).join('-'));
+    return new StaticWebsitePreviewNamingStrategy(tokens.map(({ token }) => {
+      return Token.isUnresolved(token) ? token : paramCase(token);
+    }).join('-'));
   }
 
   /**
@@ -29,6 +32,6 @@ export class StaticWebsitePreviewNamingStrategy implements IStaticWebsitePreview
   public name: string;
 
   private constructor (name: string) {
-    this.name = paramCase(name);
+    this.name = name;
   }
 }
